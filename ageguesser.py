cd2016466b2af028d162857b2896d773a5da09a7
Suzanne Szabo <suzanne.szabo@utahtech.edu>
2025-08-25 21:57:13 -0600

    first commit

---
+++ ageguesser.py
@@ -0,0 +1,20 @@
+import random
+
+
+def main():
+    print("I'm going to guess your age!")
+    name = input("First, what is your name? ")
+
+
+
+    while True:
+        guess = random.randint(15, 30)
+        response= int(input((f"Are you {guess} years old? y or n: ")))
+
+        if response == 'y':
+            print(f"{name} is {guess} years old! Yay!")
+            break
+        elif response == 'n':
+            print("Rats.")
+        else: 
+            print("Please respond with 'y' or 'n'.")
