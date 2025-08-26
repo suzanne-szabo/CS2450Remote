f88e9bad84b71e8af09640ea10aeeb8ce0e8c43e
Suzanne Szabo <suzanne.szabo@utahtech.edu>
2025-08-25 22:16:40 -0600

    Added new features: expanded age range, guess counter, no duplicate age guesses.

--- ageguesser.py
+++ ageguesser.py
@@ -5,10 +5,16 @@ def main():
     print("I'm going to guess your age!")
     name = input("First, what is your name? ")
 
-
+    min = 15
+    max = 80
+    guess_count = 0
+    guesses = list(range(min, max + 1))
 
     while True:
-        guess = random.randint(15, 30)
+        if guess_count >= 5:
+            print("I give up! I couldn't guess your age.")
+            break
+        guess = random.choice(guesses)
         response= input((f"Are you {guess} years old? y or n: "))
 
         if response == 'y':
@@ -16,6 +22,8 @@ def main():
             break
         elif response == 'n':
             print("Rats.")
+            guesses.remove(guess)
+            guess_count += 1
         else: 
             print("Please respond with 'y' or 'n'.")
 
